eab8331a5c62a4eecf49ae9f9a5cb20d2a30e8f9
pranav <pranavpriyatosh@gmail.com>
2022-08-02 15:09:09 +0530

    Initial commit

---
+++ src/main/java/com/luv2code/springboot/thymeleafdemo/ThymeleafdemoApplication.java
@@ -0,0 +1,23 @@
+package com.luv2code.springboot.thymeleafdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcProperties;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.i18n.LocaleChangeInterceptor;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+
+import java.util.Locale;
+
+@SpringBootApplication
+public class ThymeleafdemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThymeleafdemoApplication.class, args);
+	}
+
+
+}
+
